c09485435a3bd298a0340a87e1746dec78a3dc5d
jobex11 <joblawal33@gmail.com>
2024-03-29 23:15:41 +0100

    added suggestions

--- todo/app/Suggestion.js
+++ todo/app/Suggestion.js
@@ -1,7 +1,32 @@
-import React from "react";
+"use client";
+import FiberManualRecordOutlinedIcon from "@mui/icons-material/FiberManualRecordOutlined";
 
 const Suggestion = () => {
-  return <div>Suggestion</div>;
+  return (
+    <div>
+      <h1>Suggestions</h1>
+      <h5>Recently added</h5>
+      <div>
+        <div>
+          <FiberManualRecordOutlinedIcon />
+          Fix lamp
+        </div>
+        <div>
+          <FiberManualRecordOutlinedIcon />
+          Shared documents
+        </div>
+        <div>
+          <FiberManualRecordOutlinedIcon />
+          Book doctors appointment
+        </div>
+        <div>
+          <FiberManualRecordOutlinedIcon />
+          clean windows(outside)
+        </div>
+      </div>
+      is it really working
+    </div>
+  );
 };
 
 export default Suggestion;
