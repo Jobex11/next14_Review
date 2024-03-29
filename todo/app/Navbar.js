5fd39cf5abb44ed346bfa3882bfb88cce933c669
jobex11 <joblawal33@gmail.com>
2024-03-29 12:32:32 +0100

    add todo

---
+++ todo/app/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="shadow-sm p-3 text-center">
+      <h2 className="font-bold">Task Achiever</h2>
+    </div>
+  );
+};
+
+export default Navbar;
